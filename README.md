ddafefaebf9bc8eec130bc8e91942aa6d2cae1eb
Yùjǐn Lín <yujinlin0224@outlook.com>
2023-03-08 05:28:35 +0800

    update 32bit support and `iconsassoc`

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ Icon Library of MPC-HC copied from K-Lite Codec Pack (Flatro).
 ## Installation
 
 1. Download or clone the repository.
-2. Run `install.bat` with administrator privileges, or copy `mpciconlib.dll` to installation folder
-   of MPC-HC manually.
+2. Run `install.bat` with administrator privileges, or copy `32bit/mpciconlib.dll` or
+   `64bit/mpciconlib.dll` to installation folder of MPC-HC manually depending on your system
+   architecture and run `mpc-hc.exe` or `mpc-hc64.exe` with `/iconsassoc` argument to re-associate
+   format icons.
--- install.bat
+++ install.bat
@@ -1,17 +1,31 @@
 @echo off
 
-if exist "%ProgramFiles%\MPC-HC" (
-  copy /y "%~dp0\mpciconlib.dll" "%ProgramFiles%\MPC-HC"
+if exist "%ProgramFiles%\MPC-HC\mpc-hc.exe" (
+  copy /y "%~dp0\32bit\mpciconlib.dll" "%ProgramFiles%\MPC-HC"
+  start "" /wait "%ProgramFiles%\MPC-HC\mpc-hc.exe" /iconsassoc
 )
 
-if exist "%ProgramFiles(x86)%\MPC-HC" (
-  copy /y "%~dp0\mpciconlib.dll" "%ProgramFiles(x86)%\MPC-HC"
+if exist "%ProgramFiles%\MPC-HC\mpc-hc64.exe" (
+  copy /y "%~dp0\64bit\mpciconlib.dll" "%ProgramFiles%\MPC-HC"
+  start "" /wait "%ProgramFiles%\MPC-HC\mpc-hc64.exe" /iconsassoc
 )
 
-if exist "%ProgramFiles%\K-Lite Codec Pack\MPC-HC" (
-  copy /y "%~dp0\mpciconlib.dll" "%ProgramFiles%\K-Lite Codec Pack\MPC-HC"
+if exist "%ProgramFiles(x86)%\MPC-HC\mpc-hc.exe" (
+  copy /y "%~dp0\32bit\mpciconlib.dll" "%ProgramFiles(x86)%\MPC-HC"
+  start "" /wait "%ProgramFiles(x86)%\MPC-HC\mpc-hc.exe" /iconsassoc
 )
 
-if exist "%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC64" (
-  copy /y "%~dp0\mpciconlib.dll" "%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC64"
+if exist "%ProgramFiles%\K-Lite Codec Pack\MPC-HC\mpc-hc.exe" (
+  copy /y "%~dp0\32bit\mpciconlib.dll" "%ProgramFiles%\K-Lite Codec Pack\MPC-HC"
+  start "" /wait "%ProgramFiles%\K-Lite Codec Pack\MPC-HC\mpc-hc.exe" /iconsassoc
+)
+
+if exist "%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC\mpc-hc.exe" (
+  copy /y "%~dp0\32bit\mpciconlib.dll" "%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC"
+  start "" /wait "%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC\mpc-hc.exe" /iconsassoc
+)
+
+if exist "%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC64\mpc-hc64.exe" (
+  copy /y "%~dp0\64bit\mpciconlib.dll" "%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC64"
+  start "" /wait "%ProgramFiles(x86)%\K-Lite Codec Pack\MPC-HC64\mpc-hc64.exe" /iconsassoc
 )
